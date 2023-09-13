f4d32d98f24cf008d699f6b75cc2d6cc31c84864
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-13 18:06:44 +0300

    Delete Sign out for this moment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, stretches: 13
+
   validates :name, presence: true, length: { maximum: 250 }
 
-  has_many :recipes, dependent: :destroy
-  has_many :foods, dependent: :destroy
+  has_many :recipes, class_name: 'Recipe', foreign_key: 'user_id', dependent: :destroy
+  has_many :foods, class_name: 'Food', foreign_key: 'user_id', dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root 'pages#home'
-  # resources :users
+  resources :users, only: [:show, :edit]
+
 
 end
