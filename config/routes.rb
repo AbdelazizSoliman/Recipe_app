1a3f8dab53ee2a3ebb7e067eaba272a537e8fd79
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-09-14 19:55:29 +0300

    Create new recipe form

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   get '/public_recipes', to: 'recipes#public_recipes'
   delete '/recipes/:id/:food_id', to: 'recipes#remove_food_from_recipe', as: 'recipe_food'
 
-
-  resources :foods, only: [:index, :new, :create]
-  delete 'foods/:id', to: 'foods#delete', as: 'delete_food'
-
+  resources :foods, only: [:index, :new, :create, :destroy]
   
   resources :recipes, only: [:index, :new, :show, :create, :update, :destroy] do
     resources :recipe_foods, only: [:index, :show, :new, :create]
-  end
 
+      member do
+        delete :destroy
+        patch :toggle_recipe_public
+        get 'add_ingredient', to: 'recipes#add_ingredient'
+        post 'add_ingredient', to: 'recipes#add_ingredient'
+        get 'generate_shopping_list', to: 'recipes#generate_shopping_list'
+        delete 'remove_food'
+    end
+  end
+  
   root 'foods#index'
   resources :users, only: [:show, :edit]
-
-
 end
