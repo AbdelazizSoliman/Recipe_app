22bfd2163de6ca4f37ef817cc18930bf9ce4d2b4
Peter Dumbari <dummytwiz@gmail.com>
2023-09-14 13:33:23 +0100

    made some adjustment

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/public_recipes', to: 'recipes#public_recipes'
+  delete '/recipes/:id/:food_id', to: 'recipes#remove_food_from_recipe', as: 'recipe_food'
+
+
   resources :foods, only: [:index, :new, :create]
   delete 'foods/:id', to: 'foods#delete', as: 'delete_food'
 
   
-  resources :recipe,  only: [:index, :new, :show, :create, :update, :destroy] do
+  resources :recipes, only: [:index, :new, :show, :create, :update, :destroy] do
     resources :recipe_foods, only: [:index, :show, :new, :create]
   end
 
