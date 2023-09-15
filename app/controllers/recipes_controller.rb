429c96bece6906442ccca407fc6fc8ff9bb2cc2d
Peter Dumbari <dummytwiz@gmail.com>
2023-09-15 10:12:45 +0100

    modifications on public recipes file

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,107 +1,108 @@
 class RecipesController < ApplicationController
-  # before_action :authenticate_user!, except: [:show]
-
-  def index
-    @recipes = Recipe.all
-  end
-
-  def new
-    @recipe = Recipe.new
-  end
-
-  def add_ingredient
-    @recipe = Recipe.find(params[:id])
-
-    if current_user == @recipe.user
-      @foods_not_in_recipe = Food.where.not(id: @recipe.foods.pluck(:id))
-
-      if params[:recipe].present? && params[:recipe][:food_ids].present?
-        food_ids = params[:recipe][:food_ids].reject(&:empty?) # Remove empty strings
-        @recipe.foods << Food.where(id: food_ids)
-        flash[:notice] = 'Ingredients added successfully.'
-        redirect_to @recipe
-        return
+    # before_action :authenticate_user!, except: [:show]
+  
+    def index
+      @recipes = Recipe.all
+    end
+  
+    def new
+      @recipe = Recipe.new
+    end
+  
+    def add_ingredient
+      @recipe = Recipe.find(params[:id])
+  
+      if current_user == @recipe.user
+        @foods_not_in_recipe = Food.where.not(id: @recipe.foods.pluck(:id))
+  
+        if params[:recipe].present? && params[:recipe][:food_ids].present?
+          food_ids = params[:recipe][:food_ids].reject(&:empty?) # Remove empty strings
+          @recipe.foods << Food.where(id: food_ids)
+          flash[:notice] = 'Ingredients added successfully.'
+          redirect_to @recipe
+          return
+        end
+      else
+        flash[:alert] = 'You do not have permission to add ingredients to this recipe.'
       end
-    else
-      flash[:alert] = 'You do not have permission to add ingredients to this recipe.'
+  
+      render 'add_ingredient'
     end
-
-    render 'add_ingredient'
-  end
-
-  def remove_food
-    @recipe = Recipe.find(params[:id])
-    @food = Food.find(params[:food_id])
-    if current_user == @recipe.user
-      @recipe.foods.delete(@food)
-      flash[:notice] = 'Ingredient removed successfully.'
-    else
-      flash[:alert] = 'You do not have permission to remove ingredients from this recipe.'
+  
+    def remove_food
+      @recipe = Recipe.find(params[:id])
+      @food = Food.find(params[:food_id])
+      if current_user == @recipe.user
+        @recipe.foods.delete(@food)
+        flash[:notice] = 'Ingredient removed successfully.'
+      else
+        flash[:alert] = 'You do not have permission to remove ingredients from this recipe.'
+      end
+      redirect_to @recipe
     end
-    redirect_to @recipe
-  end
-
-  def public_recipes
-    @public_recipes = Recipe.where(public: true).includes(:foods)
-    @total_food_items = Food.sum(:quantity)
-    @total_price = Food.joins(:recipe_foods).sum('foods.quantity * foods.price')
-  end
-
-  def create
-    @recipe = current_user.recipes.build(recipe_params)
-    if @recipe.save
-      flash[:notice] = 'Recipe created successfully.'
-      redirect_to recipe_path(@recipe)
-    else
-      flash.now[:alert] = 'Recipe creation failed.'
-      redirect_to new_recipe_url
+  
+    def public_recipes
+        @users = User.includes(:recipes).all
+        @foods = Food.includes(:recipe_foods).all
+        @public_recipes = Recipe.includes(:user, :recipe_foods).where(public: true).order(created_at: :desc)
+        @current_user = current_user
     end
-  end
-
-  def destroy
-    @recipe = Recipe.find(params[:id])
-    if current_user == @recipe.user
-      @recipe.destroy
-      flash[:notice] = 'Recipe deleted successfully.'
-    else
-      flash[:alert] = 'You do not have permission to delete this recipe.'
+  
+    def create
+      @recipe = current_user.recipes.build(recipe_params)
+      if @recipe.save
+        flash[:notice] = 'Recipe created successfully.'
+        redirect_to recipe_path(@recipe)
+      else
+        flash.now[:alert] = 'Recipe creation failed.'
+        redirect_to new_recipe_url
+      end
     end
-    redirect_to recipes_path
-  end
-
-  def show
-    @recipe = Recipe.find(params[:id])
-    @toggle_recipe_public = params[:toggle_recipe_public]
-    return unless @toggle_recipe_public && @recipe.user == current_user
-
-    @recipe.update(public: !@recipe.public)
-    flash[:notice] = @recipe.public ? 'Recipe is now public.' : 'Recipe is now private.'
-    redirect_to recipe_path(@recipe)
-  end
-
-  def toggle_recipe_public
-    @recipe = Recipe.find(params[:id])
-
-    if @recipe.user == current_user
+  
+    def destroy
+      @recipe = Recipe.find(params[:id])
+      if current_user == @recipe.user
+        @recipe.destroy
+        flash[:notice] = 'Recipe deleted successfully.'
+      else
+        flash[:alert] = 'You do not have permission to delete this recipe.'
+      end
+      redirect_to recipes_path
+    end
+  
+    def show
+      @recipe = Recipe.find(params[:id])
+      @toggle_recipe_public = params[:toggle_recipe_public]
+      return unless @toggle_recipe_public && @recipe.user == current_user
+  
       @recipe.update(public: !@recipe.public)
       flash[:notice] = @recipe.public ? 'Recipe is now public.' : 'Recipe is now private.'
-    else
-      flash[:alert] = 'You do not have permission to toggle this recipe.'
+      redirect_to recipe_path(@recipe)
     end
-
-    redirect_to @recipe
-  end
-
-  def generate_shopping_list
-    @recipe = Recipe.find(params[:id])
-
-    @required_foods = @recipe.foods.where.not(user: current_user).includes(:user)
-    @total_value = @required_foods.sum { |food| food.quantity * food.price }
-  end
-
-  private
-
-  def recipe_params
-    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
-  end
+  
+    def toggle_recipe_public
+      @recipe = Recipe.find(params[:id])
+  
+      if @recipe.user == current_user
+        @recipe.update(public: !@recipe.public)
+        flash[:notice] = @recipe.public ? 'Recipe is now public.' : 'Recipe is now private.'
+      else
+        flash[:alert] = 'You do not have permission to toggle this recipe.'
+      end
+  
+      redirect_to @recipe
+    end
+  
+    def generate_shopping_list
+      @recipe = Recipe.find(params[:id])
+  
+      @required_foods = @recipe.foods.where.not(user: current_user).includes(:user)
+      @total_value = @required_foods.sum { |food| food.quantity * food.price }
+    end
+  
+    private
+  
+    def recipe_params
+      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_13_083244) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_15_042040) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,10 +40,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_13_083244) do
     t.integer "preparation_time"
     t.string "cooking_time"
     t.string "description"
-    t.string "public"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "public", default: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
