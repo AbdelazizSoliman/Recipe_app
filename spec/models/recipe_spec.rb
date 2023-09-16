88518ede188eb01e9ee056a82e6d74e8faa08194
Peter Dumbari <dummytwiz@gmail.com>
2023-09-16 18:12:01 +0100

    add test

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,30 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.create!(name: 'Christian Momo', email: 'mm@recipapp.com', password: '123456') }
+  let(:recipe) do
+    user.recipes.create!(name: 'recipe 1', description: 'recipe description', cooking_time: 10, preparation_time: 5)
+  end
+
+  describe 'validation' do
+    it 'requires name to be present' do
+      recipe.name = nil
+      expect(recipe).not_to be_valid
+    end
+
+    it 'requires description to be present' do
+      recipe.description = nil
+      expect(recipe).not_to be_valid
+    end
+
+    it 'requires cooking_time to be positive number' do
+      recipe.cooking_time = -1
+      expect(recipe).not_to be_valid
+    end
+
+    it 'requires preparation_time to be positive number' do
+      recipe.preparation_time = -1
+      expect(recipe).not_to be_valid
+    end
+  end
 end
